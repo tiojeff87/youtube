b1c49b3673143df2f0495682b8a6d0cae12a3aff
tiojeff87 <tiojeff87@gmail.com>
2024-04-19 14:34:35 +0100

    Add project files.

---
+++ Youtube.Domain/Entities/Youtuber.cs
@@ -0,0 +1,15 @@
+﻿namespace Youtube.Domain.Entities
+{
+    public class Youtuber
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int Subscribers { get; set; }
+        public int videos { get; set; }
+        public int views { get; set; }
+        public DateTime Joined { get; set; }
+        public string? country { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
